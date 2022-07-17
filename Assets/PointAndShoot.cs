ca34b4728eb053d457a5f34b44104b8ca56e7599
Alegator8 <92559494+Alegator8@users.noreply.github.com>
2022-07-17 16:40:30 -0700

    i lost track

---
+++ Assets/PointAndShoot.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndShoot : MonoBehaviour
+{
+    private Vector3 target;
+
+    public GameObject crosshair;
+    public GameObject player;
+    public GameObject bulletPrefab;
+    public GameObject bulletStart;
+
+    public float bulletSpeed = 60.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshair.transform.position = new Vector2(target.x, target.y);
+
+        Vector3 difference = target - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+           player.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+        }
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            fireBullet(direction, rotationZ);
+        }
+
+        void fireBullet(Vector2 direction, float rotationZ)
+        {
+            GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+            bullet.transform.position = bulletStart.transform.position;
+            bullet.transform.rotation = Quaternion.Euler(0f, 0f, rotationZ);
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+        }
+    }
+}
