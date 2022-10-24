663da0813d72dacff10a9fd7c738faec08d06799
Eric Park <pjheric1219@gmail.com>
2022-10-24 13:24:03 -0700

    buildable

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Search;
 using UnityEngine.UI; 
 using UnityEngine.SceneManagement;
 
